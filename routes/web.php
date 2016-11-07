a753da679957f0255a7f2cadf07d3c8f74d0acb7
Quan Do <manhquan.do@ved.com.vn>
2016-11-07 07:53:48 +0000

    init

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+#Admin Routes
+Route::get('admin/login', 'AdminController@redirectToGoogle');
+Route::get('admin/logout', 'AdminController@logout');
+Route::get('admin/callback', 'AdminController@handleGoogleCallback');
+Route::get('admin/notice', 'AdminController@notice');
+Route::get('admin', 'AdminController@index');
+#Content Routes
+foreach (config('site.content') as $content => $config) {
+    Route::resource('admin/'.$content, 'ContentsController');
+}
